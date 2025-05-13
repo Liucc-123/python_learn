191b8a927d672957fd4a2ade2f2a2b6283ca73a7
liuchuangchuang <2514573000@qq.comeval>
2025-05-13 23:04:12 +0800

    项目初始化

---
+++ 一、Python 语言描述/转义字符.py
@@ -0,0 +1,22 @@
+# \t制表符
+print("jack\t20")
+
+# \n换行
+print("Hello,jack\nhello,tom")
+
+# \\输出\
+print("D:\\Pycharm\\chapter02")
+
+# \"输出"     \'输出'
+print("郭靖说：\"hello\"")
+print("郭靖说：\'hello\'")  # 单引号也可以不使用转义 \
+
+# \r回车
+print("不是一个人的王者, \r而是团队的荣耀")
+
+"""
+输出如下文案
+姓名	年龄	籍贯	住址
+tom	12	河北	北京
+"""
+print("姓名\t年龄\t籍贯\t住址\ntom\t12\t河北\t北京")
